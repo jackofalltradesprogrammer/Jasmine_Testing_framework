49c885181edee5ca090f6aec6bd774add01511d5
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-31 15:25:48 -0400

    Avoiding exact equality with jasmin.any - It returns true if not null or undefined

--- spec/mySpec.js
+++ spec/mySpec.js
@@ -208,3 +208,26 @@ describe("Multiplie spies, when created manually", function(){
         expect(tape.rewind).toBeDefined();
     });
 });
+
+describe("Matching with finesse", function(){
+
+    describe("jasmine any", function(){
+        it("matches any value", function(){
+            expect({}).toEqual(jasmine.any(Object));
+            expect(12).toEqual(jasmine.any(Number));
+        });
+
+        describe("when used with a spy", function(){
+            it("is useful for comparing arguments", function(){
+                var foo = jasmine.createSpy("foo");
+                foo(12,function(){
+                    return true;
+                });
+            expect(foo).toHaveBeenCalledWith(jasmine.any(Number),jasmine.any(Function));
+            });
+        });
+    });
+
+    
+});
+
