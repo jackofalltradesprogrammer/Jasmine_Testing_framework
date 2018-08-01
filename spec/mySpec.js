2a6551334c15acdb3eb1fce8374bc13fe0fffb7e
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-08-01 15:19:59 -0400

    To check that it meets a certain criteria, asymmetric equality tester

--- spec/mySpec.js
+++ spec/mySpec.js
@@ -321,5 +321,28 @@ describe("Matching with finesse", function(){
             });
         });
     });
+
+    describe("custom asymmetry", function(){
+        var tester = {
+            asymmetricMatch: function(actual) {
+                var secondValue = actual.split(',')[1];
+                return secondValue === 'bar';
+            }
+        };
+
+        it("dives in deep", function(){
+            expect("foo,bar,baz,quux").toEqual(tester);
+        });
+        
+        describe("when used with a spy", function(){
+            it("is useful for comparing arguments", function(){
+                var callback = jasmine.createSpy('callback');
+
+                callback('foo,bar,baz');
+
+                expect(callback).toHaveBeenCalledWith(tester);
+            });
+        });
+    });
 });
 
