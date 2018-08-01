83450ccdef49acf08e8a4d332c5cdcab7d1f9162
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-08-01 14:06:38 -0400

    jasmine.stringMatching is to match a portion of string with the spec

--- spec/mySpec.js
+++ spec/mySpec.js
@@ -303,5 +303,23 @@ describe("Matching with finesse", function(){
             });
         });
     });
+
+    describe("jasmine.stringMatching", function() {
+        it("matches as a regexp", function(){
+            expect({foo:'bar'}).toEqual({foo:jasmine.stringMatching(/^bar$/)});
+            expect({foo:'foobarbaz'}).toEqual({foo:jasmine.stringMatching('barb')});
+        });
+
+        describe("when used with a spy", function(){
+            it("is useful for comparing arguments", function(){
+                var callback = jasmine.createSpy("callback");
+
+                callback("foobarbaz");
+
+                expect(callback).toHaveBeenCalledWith(jasmine.stringMatching('bar'));
+                expect(callback).not.toHaveBeenCalledWith(jasmine.stringMatching(/^bar$/));
+            });
+        });
+    });
 });
 
